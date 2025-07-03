34e9c0f826c577aa2dcdcdb73a4f1b35669132e6
Gabriel <gabriel@mazure.fr>
2025-07-03 19:01:03 +0200

    initial (D) commit

---
+++ car.c
@@ -0,0 +1,9 @@
+//
+// Created by gabri on 03/07/2025.
+//
+
+#include "car.h"
+
+void compute_body_positions(car* car) {
+
+}
---
+++ car.h
@@ -0,0 +1,29 @@
+//
+// Created by gabri on 03/07/2025.
+//
+
+#ifndef CAR_H
+#define CAR_H
+#include "raylib.h"
+
+typedef struct body {
+    Vector2 frontLeft;
+    Vector2 frontRight;
+    Vector2 backLeft;
+    Vector2 backRight;
+} body;
+
+typedef struct relativePositions {
+    Vector2 fLtoBl;
+} relativePositions;
+
+typedef struct car {
+    body body;
+    relativePositions relativePositions;
+    Vector2 centerPos;
+    double distCFL; //distance between center point and front left corner
+    double angle;
+} car;
+
+void compute_body_positions(car* car);
+#endif //CAR_H
