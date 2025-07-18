199fa353be466dd9e4a2292e1c0ed9fdcc63a94a
Gabriel <gabriel@mazure.fr>
2025-07-18 18:43:42 +0200

    Revert "Added lateral force of the tyres which is computed using Pacejka's magic formula"
    This reverts commit a0de799b35169bda0fe4fbc1e3606c7dfffa8103.

--- main.c
+++ main.c
@@ -28,15 +28,15 @@ int main(void) {
 
         //Handling imputs
         if (IsKeyDown(KEY_LEFT)) {
-            car->angle -= 0.03f;
+            car->angle -= 0.05f;
             if (car->wheels.FwheelAngle > -0.6f) {
-                car->wheels.FwheelAngle -= 0.02f;
+                car->wheels.FwheelAngle -= 0.05f;
             }
         }
         if (IsKeyDown(KEY_RIGHT)) {
-            car->angle += 0.03f;
+            car->angle += 0.05f;
             if (car->wheels.FwheelAngle < 0.6f) {
-                car->wheels.FwheelAngle += 0.02f;
+                car->wheels.FwheelAngle += 0.05f;
             }
         }
 
